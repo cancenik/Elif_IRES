f01ca87b36148690fd8e23d5dcb3d85bd8c05898
Can Cenik <cancenik@dn52empm.sunet>
2014-07-24 14:21:40 -0700

    added hek analysis

--- Main-Analysis.R
+++ Main-Analysis.R
@@ -2,6 +2,7 @@ library(edgeR)
 dat = read.csv ('~/elif_ires//072314_Elif_comparison_ALLDATA.csv')
 dat[,-1] = log10(dat[,-1])
 hek_ires = read.csv('~/elif_ires/072214_HEK_allreplicates_deleted_rows.csv')
+hek_ires[,-1] = log10(hek_ires[,-1])
 
 # test whether mean ratio is the across all cell lines
 # We can use either kruskal-wallis non-parametric or aov for parametric assumption
@@ -17,11 +18,35 @@ length(kruskal_pvals)
 # a1 = aov(as.numeric(dat[5,-1]) ~ as.factor(cell_type))
 aov_pvals = apply ( dat[,-1], 1, function (x) {summary.aov(aov(as.numeric(x) ~ as.factor(cell_type)))[[1]][1,5] } )
 hist(aov_pvals,20, xlim = c(0,1))
-length(which(aov_pvals < 0.05))
+length(which(aov_pvals < 0.01))
 length(aov_pvals)
 # summary.aov ( a1)
+#> length(which(aov_pvals < 0.01))
+#[1] 256
+#> length(aov_pvals)
+#[1] 278
 # TukeyHSD ( a1)
 # boxplot (as.numeric(dat[5,-1]) ~ as.factor(cell_type))
 pdf ('~/elif_ires/FIGURES/MDS_Replicate_similarity_tissues.pdf', width=5, height=5)
 plotMDS(dat[,-1])
 dev.off()
+
+
+# Test which genes have higher activity than EMCV or HCV in hek_ires dataset
+emcv_hek = as.numeric(hek_ires[251,-1])
+hcv_hek = as.numeric(hek_ires[97, -1])
+
+test_emcv = function (y){ t.test(emcv_hek, as.numeric(y), alternative="less")$p.value }
+emcv_pvalue = apply ( hek_ires[,-1], 1, test_emcv )
+hist ( emcv_pvalue)
+length ( which(emcv_pvalue < .05))
+length(emcv_pvalue )
+# 113 / 279 =  0.4050179
+
+test_hcv = function (y){ t.test(hcv_hek, as.numeric(y), alternative="less")$p.value }
+hcv_pvalue =  apply ( hek_ires[,-1], 1, test_hcv )
+hist( hcv_pvalue)
+length( which(hcv_pvalue < .05) )
+length(hcv_pvalue)
+# 75/ 279 = [1] 0.2688172
+
